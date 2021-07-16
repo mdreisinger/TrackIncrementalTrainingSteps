9ef61fcc880a7f39bfe57d5a0be52f2b0139ab3a
Michael Dreisinger <mike.dreisinger95@gmail.com>
2021-07-16 00:56:59 +0000

    create migration repo

--- TrackIncrementalTrainingSteps/models.py
+++ TrackIncrementalTrainingSteps/models.py
@@ -1,8 +1,8 @@
-from app import db
+from TrackIncrementalTrainingSteps import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Strings(64), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
