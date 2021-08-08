771d3ed98419544b2ae36a45601c6535695a6033
tim rotzinger <tim.rotzinger@gmail.com>
2021-08-08 19:17:17 +0000

    chapter 6 complete

--- TrackIncrementalTrainingSteps/models.py
+++ TrackIncrementalTrainingSteps/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,6 +13,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     workouts = db.relationship("Workout", backref="user", lazy="dynamic")
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -22,11 +25,15 @@ class User(UserMixin, db.Model):
     def check_password_hash(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
 
 class Workout(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    rest_period = db.Column(db.Time)
+    rest_period = db.Column(db.DateTime)
     sets = db.relationship("Set", backref="workout", lazy="dynamic")
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
         
